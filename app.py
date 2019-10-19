358dc7549ec8859c08d30c09e6dcd4a92aff56aa
Shakeel <z5172568@ad.unsw.edu.au>
2019-10-19 14:12:28 +1100

    working on login functions

--- app.py
+++ app.py
@@ -2,10 +2,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
+users = []
+
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/user/login', method=['POST'])
+def user_login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    for i in range(0,len(users)):
+        if username == users[i]['username'] and password == users[i]['password']:
+            return render_template("./")
+
+    return "Invalid username or password"
 
 
 if __name__ == '__main__':

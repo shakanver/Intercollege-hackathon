80db18fd8699ec2e7e0984a35e03d72de9d8e161
Shakeel <z5172568@ad.unsw.edu.au>
2019-10-19 14:44:13 +1100

    successfully added login feature

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-users = []
+users = [{"username": "shakeel", "password": "shak28"}]
 
 
 
@@ -10,23 +10,17 @@ users = []
 def home():
     return render_template("./register.html")
 
-@app.route('/user/login', method=['POST'])
-def user_login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    for i in range(0,len(users)):
-        if username == users[i]['username'] and password == users[i]['password']:
-            return render_template("./")
-
-    return "Invalid username or password"
-
-
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
+
     print(username , password)
-    return render_template("./test.html")
+    for i in range(0,len(users)):
+        if username == users[i]['username'] and password == users[i]['password']:
+            return render_template("/test.html")
+
+    return "Invalid username or password"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port = 2512)

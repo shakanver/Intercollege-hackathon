c932eb23485a095f56028e3a9908e4b5fb80df8c
Farhan Ghazi <farhan.ghazi@macrovue.com.au>
2019-10-19 14:17:30 +1100

    added templates

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# register (username , password) return: url to profile
+# send_message (thread, timestamp, content) return: boolean
+# get_messages() return: messages[]
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template("./register.html")
 
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    print(username , password)
+    return render_template("./test.html")
 
 if __name__ == '__main__':
     app.run(debug=True)

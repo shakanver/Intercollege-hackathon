0dc66b775378aa5f900887a0ecb06619844d2b0a
Farhan Ghazi <farhan.ghazi@macrovue.com.au>
2019-10-19 17:13:09 +1100

    login takes in more data

--- app.py
+++ app.py
@@ -5,19 +5,21 @@ app = Flask(__name__)
 users = []
 messages = []
 
-
 @app.route('/register',methods=['GET', 'POST'])
 def register():
 
     if request.method == 'GET':
         return render_template('register.html')
     if request.method == 'POST':
+
         username = request.form['username']
         password = request.form['password']
         college = request.form['college']
         degree = request.form['degree']
         year = request.form['year']
+
         user = {'username': username, 'password': password, 'college': college, 'degree': degree, 'year':year}
+
         if user not in users:
             users.append(user)
             print(users)
@@ -27,8 +29,10 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
     if request.method == 'GET':
         return render_template("/login.html")
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -52,8 +56,6 @@ def send_message():
 @app.route('/messages',methods = ['GET'])
 def view_messages():
     return render_template("/messages.html")
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, port = 2512)
